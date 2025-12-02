1375ed14beb9d6255f383bb40864e927b99ba004
Ram0kr0singh <ramkumar8920931924@gmail.com>
2025-12-02 22:00:00 +0530

    Async Await Fetch 2.0

--- 03 JS/51 Async Await Fetch API/script.js
+++ 03 JS/51 Async Await Fetch API/script.js
@@ -1,55 +1,78 @@
-// async function getdata() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve(500)
-//         }, 3500);
-//     })
-// }
+// // async function getdata() {
+// //     return new Promise((resolve, reject) => {
+// //         setTimeout(() => {
+// //             resolve(500)
+// //         }, 3500);
+// //     })
+// // }
 
-// async function main() {
-//         console.log("Loading something");
+// // async function main() {
+// //         console.log("Loading something");
 
-//         console.log("Loading something 2");
+// //         console.log("Loading something 2");
 
-//         console.log("Load data");
+// //         console.log("Load data");
 
-//         let data = await getdata()
+// //         let data = await getdata()
 
-//         console.log(data);
+// //         console.log(data);
 
-//         console.log("process data");
+// //         console.log("process data");
 
-//         console.log("tast 2");
+// //         console.log("tast 2");
+// //     }
+
+// // main()
+
+
+// async function getdata2() {
+//         let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         let data2 = await x.json()
+//         return data2
 //     }
 
-// main()
+// async function main2() {
+//         console.log("2Loading something");
 
+//         console.log("2Loading something 2");
 
-async function getdata2() {
-        let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-        let data2 = await x.json()
-        return data2
-    }
+//         console.log("2Load data");
 
-async function main2() {
-        console.log("2Loading something");
+//         let data22 = await getdata2()
+//         console.log(data22);
+//         console.log("2process data");
 
-        console.log("2Loading something 2");
+//         console.log("2last 2");
+//     }
+// main2()
+// // data.then((v) =>{
+// // console.log(data);
 
-        console.log("2Load data");
+// // console.log("process data");
 
-        let data22 = await getdata2()
-        console.log(data22);
-        console.log("2process data");
+// // console.log("tast 2");
 
-        console.log("2last 2");
-    }
-main2()
-// data.then((v) =>{
-// console.log(data);
+// // })
 
-// console.log("process data");
+// __________________________________________________
 
-// console.log("tast 2");
+async function getData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(500)
+        }, 1500);
+    })
+}
 
-// })
+async function main() {
+    console.log("Loading Module")
+    console.log("Loading Module")
+    console.log("Loading Module")
+
+    let data = await getData()
+    console.log(data)
+
+    console.log("task 2 Module")
+}
+
+main()
